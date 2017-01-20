25d06ef9f9cbeda04059e3d3988f42f0995d6614
feitian124 <feitian124@qq.com>
2017-01-21 01:32:24 +0800

    first commit

---
+++ src/main/java/Char.java
@@ -0,0 +1,49 @@
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.UnsupportedEncodingException;
+
+/**
+ * chinese char
+ */
+public class Char {
+	
+	private static final int UNICODE_MIN = 19968;
+	
+	private static final int UNICODE_MAX = 40869;
+	
+	private static final String CR = System.getProperty("line.separator");
+	
+	public void execute(String fileName) throws IOException {
+		File f = new File(fileName);
+		FileWriter fw = new FileWriter(f);
+		System.out.println("FileWriter getEncoding: " + fw.getEncoding());
+		
+		fw.write("字符, Unicode十进制, Unicode十六进制, GBK十进制, GBK十六进制" + CR);
+
+		for (int i = UNICODE_MIN; i <= UNICODE_MAX; i++) {
+			int gbkCode = getGBKCode(i);
+			String line = String.format("%s, %s, %s, %s, %s", (char)i, i, toHex(i), gbkCode, toHex(gbkCode));
+			fw.write(line + CR);
+		}
+		
+		fw.flush();
+		fw.close();
+		
+		System.out.println(f.getAbsolutePath());
+	}
+
+	private int getGBKCode(int unicode) throws UnsupportedEncodingException {
+		char c = (char) unicode;
+		byte[] bytes = (c + "").getBytes("GBK");
+		return ((bytes[0] & 255) << 8) + (bytes[1] & 255);
+	}
+	
+	private String toHex(int i) {
+		return Integer.toHexString(i);
+	}
+
+	public static void main(String[] args) throws Exception {
+		new Char().execute("汉字编码一览表.txt");
+	}
+}
