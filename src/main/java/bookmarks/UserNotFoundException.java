4bfcdef64a9a178cf7938acaee9ff41eecc7fa34
feitian124 <feitian124@qq.com>
2017-02-22 00:45:43 +0800

    ok

---
+++ src/main/java/bookmarks/UserNotFoundException.java
@@ -0,0 +1,17 @@
+package bookmarks;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+/**
+ * @author Greg Turnquist
+ */
+// tag::code[]
+@ResponseStatus(HttpStatus.NOT_FOUND)
+class UserNotFoundException extends RuntimeException {
+
+	public UserNotFoundException(String userId) {
+		super("could not find user '" + userId + "'.");
+	}
+}
+// end::code[]
---
+++ src/main/java/com/yunnuy/GoodnameApplication.java
@@ -0,0 +1,12 @@
+package com.yunnuy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+//@SpringBootApplication
+public class GoodnameApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GoodnameApplication.class, args);
+	}
+}
